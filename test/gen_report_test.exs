81f0403f71753800ac5425dbd47922fea9f38dec
Rômulo Silva <romulo_lss@hotmail.com>
2021-09-26 18:25:18 -0300

    tests: add parallel reports

--- test/gen_report_test.exs
+++ test/gen_report_test.exs
@@ -6,6 +6,8 @@ defmodule GenReportTest do
 
   @file_name "gen_report.csv"
 
+  @file_list ["part_1.csv", "part_2.csv", "part_3.csv"]
+
   describe "build/1" do
     test "When passing file name return a report" do
       response = GenReport.build(@file_name)
@@ -17,6 +19,20 @@ defmodule GenReportTest do
       response = GenReport.build()
 
       assert response == {:error, "Insira o nome de um arquivo"}
+    end
+  end
+
+  describe "build_from_many/1" do
+    test "When a file list is provided, returns a report" do
+      response = GenReport.build_from_many(@file_list)
+
+      assert response == ReportFixture.build_from_many()
+    end
+
+    test "When a file list was not provided, returns an error" do
+      response = GenReport.build_from_many()
+
+      assert response == {:error, "Please, provide a list of strings."}
     end
   end
 end
