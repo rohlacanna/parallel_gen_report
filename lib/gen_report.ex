c313fe97b18d91edf94edffad27b4db637426aa6
Rômulo Silva <romulo_lss@hotmail.com>
2021-09-26 18:18:45 -0300

    refac: split file parser into other module

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,4 +1,6 @@
 defmodule GenReport do
+  alias GenReport.Parser
+
   @workers [
     "Cleiton",
     "Daniele",
@@ -13,19 +15,13 @@ defmodule GenReport do
   ]
 
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.reduce(report_acc(), fn line, report ->
-      [worker, worked_hours, _day, _month, _year] = parse_line(line)
-      Map.put(report, worker, report[worker] + worked_hours)
-    end)
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_hours(line, report) end)
   end
 
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(1, &String.to_integer/1)
+  defp sum_hours([worker, worked_hours, _day, _month, _year], report) do
+    Map.put(report, worker, report[worker] + worked_hours)
   end
 
   defp report_acc, do: Enum.into(@workers, %{}, &{&1, 0})
