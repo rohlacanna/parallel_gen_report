4c654efc1050663e8ccb3b929a4da83509362633
Rômulo Silva <romulo_lss@hotmail.com>
2021-09-26 18:19:26 -0300

    refac: change month name

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,4 +1,19 @@
 defmodule GenReport.Parser do
+  @month_name %{
+    "1" => "janeiro",
+    "2" => "fevereiro",
+    "3" => "março",
+    "4" => "abril",
+    "5" => "maio",
+    "6" => "junho",
+    "7" => "julho",
+    "8" => "agosto",
+    "9" => "setembro",
+    "10" => "outubro",
+    "11" => "novembro",
+    "12" => "dezembro"
+  }
+
   def parse_file(filename) do
     "reports/#{filename}"
     |> File.stream!()
@@ -9,6 +24,10 @@ defmodule GenReport.Parser do
     line
     |> String.trim()
     |> String.split(",")
+    |> List.update_at(0, &String.downcase/1)
     |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, fn month -> @month_name[month] end)
+    |> List.update_at(4, &String.to_integer/1)
   end
 end
