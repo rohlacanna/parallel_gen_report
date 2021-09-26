a486000d840cc49491222a11aae4d28764e5d539
Rômulo Silva <romulo_lss@hotmail.com>
2021-09-26 18:10:46 -0300

    Initial commit

---
+++ lib/gen_report.ex
@@ -0,0 +1,18 @@
+defmodule GenReport do
+  @moduledoc """
+  Documentation for `GenReport`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenReport.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
